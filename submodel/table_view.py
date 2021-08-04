67da7c9439af32a62026c3e47add09fce41bc2bc
曾鹏飞 <zpfkufei@gmail.com>
2021-08-04 20:37:51 +0800

    支持编辑

--- submodel/table_view.py
+++ submodel/table_view.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore
+from PyQt5.QtCore import QModelIndex, Qt
 
 
 class LocallyTableModel(QtCore.QAbstractTableModel):
@@ -26,26 +27,22 @@ class LocallyTableModel(QtCore.QAbstractTableModel):
         row = index.row()
         column = index.column()
 
-        # [[1, 'F24', '原图', None], [2, 'F20', '鲜明', 'MK11'], [3, 'F23', '暗调', 'MK11'], [4, 'F07', '金属', 'MK07'],
-        #  [5, 'F26', '尼龙', 'MK14'], [6, 'F31', '海街', 'MK01'], [7, 'F10', '昭和', 'MK09'], [8, 'F30', '美国派', 'MK13'],
-        #  [9, 'F33', '春夏', 'MK05']]
         if role == QtCore.Qt.DisplayRole:
             row_data = self._data[row]
+            if column == 0:
+                return row_data[0]
+            else:
+                if row_data[column] is None:
+                    return ""
+                else:
+                    return str(row_data[column])
+                pass
+            pass
+        pass
 
-
-    # if row < len(self._data):
-    #     columns = self._data[row]
-    #     print(columns)
-
-    # if role == QtCore.Qt.DisplayRole:
-    #     if column == 0:
-    #         return self._data[row].id
-    #     else:
-    #         box = QtWidgets.QLineEdit()
-    #         box.setText('X')
-    #
-    #         return [box]
-    #     pass
-    # pass
-
-    pass
+    def flags(self, index: QModelIndex):
+        column = index.column()
+        if column == 0:
+            return Qt.ItemIsSelectable | Qt.ItemIsEnabled
+        else:
+            return Qt.ItemIsSelectable | Qt.ItemIsEnabled | Qt.ItemIsEditable
